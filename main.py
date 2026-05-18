e01131d1fd64fb37cd8ff2679ee568d36d241653
Mbi Shanto <mbishanto25@gmail.com>
2026-05-18 21:07:25 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify
+import google.generativeai as genai
+import os
+
+app = Flask(__name__)
+
+# API KEY
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+
+    user_message = request.json["message"]
+
+    response = model.generate_content(user_message)
+
+    return jsonify({
+        "reply": response.text
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=81)
