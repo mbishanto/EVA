fb850f5ac1184fc1297ded2de9eb297a1c07409d
Mbi Shanto <mbishanto25@gmail.com>
2026-05-19 18:04:35 +0600

    Update and rename style.css to script.js

---
+++ frontend/script.js
@@ -0,0 +1,49 @@
+const API_URL = "PASTE_RENDER_BACKEND_URL_HERE";
+
+async function sendMessage(){
+
+    const input = document.getElementById("message");
+    const chatBox = document.getElementById("chat-box");
+
+    const message = input.value;
+
+    if(message.trim() === ""){
+        return;
+    }
+
+    // USER MESSAGE
+    const userDiv = document.createElement("div");
+
+    userDiv.className = "message user";
+    userDiv.innerText = message;
+
+    chatBox.appendChild(userDiv);
+
+    input.value = "";
+
+    // SEND TO BACKEND
+    const response = await fetch(API_URL + "/chat",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+            message:message
+        })
+    });
+
+    const data = await response.json();
+
+    // BOT MESSAGE
+    const botDiv = document.createElement("div");
+
+    botDiv.className = "message bot";
+    botDiv.innerText = data.reply;
+
+    chatBox.appendChild(botDiv);
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
