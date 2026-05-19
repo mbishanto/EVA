52a57ebe46733ededc38ce453bdb67f89e565d0e
Mbi Shanto <mbishanto25@gmail.com>
2026-05-19 18:25:48 +0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 @app.route("/")
 def home():
